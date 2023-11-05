bced2142394ddb50691880ef2a25247c07cdc5dc
MQInfer12 <maummq@gmail.com>
2023-11-05 14:22:40 -0400

    login

---
+++ client/src/components/Button.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let handleClick;
+</script>
+
+<button on:click={handleClick}>
+  <slot />
+</button>
+
+<style>
+  button {
+    padding: 10px;
+    background-color: var(--blue);
+    border: none;
+    color: var(--white);
+    border-radius: 12px;
+    font-weight: 700;
+    font-size: 16px;
+    transition: opacity 0.3s;
+    cursor: pointer;
+  }
+  button:hover {
+    opacity: 0.8;
+  }
+  button:disabled {
+    filter: grayscale(1);
+    pointer-events: none;
+  }
+</style>

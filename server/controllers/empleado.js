fb24d321213dc3a8c6dc312a4901f44cd30ccc4d
JoshVComstock <josedanielveizaga@gmail.com>
2023-12-03 12:09:21 -0400

    encriptado de datos

--- server/controllers/empleado.js
+++ server/controllers/empleado.js
@@ -1,6 +1,6 @@
-import express from "express"
-import { PrismaClient } from "@prisma/client"
-
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 const app = express();
 const prisma = new PrismaClient();
 
@@ -8,95 +8,104 @@ app.get("/admin", async (req, res) => {
   try {
     let empleado = await prisma.empleado.create({
       data: {
-        nombre: "Admin",
-        password: "123456",
-        usuario: "admin",
-        turno: "Tarde"
-      }
+        nombre: "jose",
+        password: await bcrypt.hash(req.body.password, 10),
+        usuario: "jose",
+        turno: "Tarde",
+      },
     });
     res.json({
       data: empleado,
-      message: "Empleado agregado correctamente"
+      message: "Empleado agregado correctamente",
     });
   } catch (error) {
     res.status(500).json({
       message: "Error al crear empleado",
-      error: error.message
-    })
+      error: error.message,
+    });
   }
 });
 
 app.get("/empleado", async (req, res) => {
   try {
-    const empleado = await prisma.Empleado.findMany({});
+    const empleado = await prisma.empleado.findMany({
+      select: {
+        id: true,
+        nombre: true,
+        usuario: true,
+        turno: true,
+        activo: true,
+      },
+    });
     res.json({
       data: empleado,
-      message: "Empleado obtenido correctamente"
-    })
+      message: "Empleado obtenido correctamente",
+    });
   } catch (error) {
     res.status(500).json({
       message: "Error al obtener empleado",
-      error: error.message
-    })
+      error: error.message,
+    });
   }
 });
 
 app.post("/empleado", async (req, res) => {
   try {
+    const password = req.body.password;
+    const hashedPassword = await bcrypt.hash(password, 10);
     let empleado = await prisma.empleado.create({
-      data: req.body
+      data: {
+        ...req.body,
+        password: hashedPassword,
+      },
     });
     res.json({
       data: empleado,
-      message: "Empleado agregado correctamente"
-    })
+      message: "Empleado agregado correctamente",
+    });
   } catch (error) {
     res.status(500).json({
       message: "Error al crear empleado",
-      error: error.message
-    })
+      error: error.message,
+    });
   }
-})
+});
 app.put("/empleado/:id", async (req, res) => {
   try {
     const empleado = await prisma.empleado.update({
       where: {
-        id: Number(req.params.id)
+        id: Number(req.params.id),
       },
-      data: req.body
-    })
-    res.json(
-      {
-        data: empleado,
-        message: "Empleado actualizado correctamente"
-      }
-    );
+      data: req.body,
+    });
+    res.json({
+      data: empleado,
+      message: "Empleado actualizado correctamente",
+    });
   } catch (error) {
     res.status(500).json({
       message: "Error al actualizar empleado",
-      error: error.message
-    })
+      error: error.message,
+    });
   }
-})
+});
 app.delete("/empleado/:id", async (req, res) => {
   try {
     const empleado = await prisma.empleado.delete({
       where: {
-        id: Number(req.params.id)
-      }
-    })
+        id: Number(req.params.id),
+      },
+    });
     res.json({
       data: empleado,
-      message: "Empleado delete correctamente"
-    }
-    );
+      message: "Empleado delete correctamente",
+    });
   } catch (error) {
     res.status(500).json({
       message: "Error al eliminar empleado",
-      error: error.message
-    })
+      error: error.message,
+    });
   }
-
-})
+});
 
 export default app;
--- server/controllers/venta.js
+++ server/controllers/venta.js
@@ -1,35 +1,93 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/venta", async (req,res) => {
+app.get("/venta", async (req, res) => {
   try {
     const venta = await prisma.venta.findMany({});
     res.json({
       data: venta,
-      message: "ventas obtenidas correctamente"
-    })
+      message: "ventas obtenidas correctamente",
+    });
   } catch (error) {
     res.status(500).json({
-      message: "Error al obtener las ventas", error: error.message
-    })
+      message: "Error al obtener las ventas",
+      error: error.message,
+    });
   }
-})
+});
 app.post("/venta", async (req, res) => {
   try {
+    const productos = await prisma.producto.findMany({
+      where: {
+        id: {
+          in: req.body.productos.map((producto) => producto.id),
+        },
+      },
+    });
+    let flag = false;
+    req.body.productos.forEach((producto) => {
+      const productoExistente = productos.find(
+        (prod) => prod.id === producto.id
+      );
+      if (producto.cantidad > productoExistente.cantidad) {
+        flag = true;
+        res.json({
+          message: "Error",
+          error:
+            "la cantidad de " +
+            productoExistente.nombre +
+            " no puede ser mayor a la existente",
+        });
+      }
+    });
+    if (flag) return;
+    const total = productos.reduce((suma, producto) => {
+      const productoVendido = req.body.productos.find(
+        (prod) => prod.id === producto.id
+      );
+      const totalProducto = producto.precio * productoVendido.cantidad;
+      suma += totalProducto;
+      return suma;
+    }, 0);
     const venta = await prisma.venta.create({
-      data: req.body
-    })
+      data: {
+        empleadoId: req.body.empleado,
+        total: total - req.body.descuento,
+        DetalleVenta: {
+          createMany: {
+            data: req.body.productos.map((producto) => ({
+              cantidad: producto.cantidad,
+              precioVendido: producto.precioVendido,
+              productoId: producto.id,
+              total: producto.precioVendido * producto.cantidad
+            })),
+          },
+        },
+      },
+    });
+    req.body.productos.forEach(async (producto) => {
+      await prisma.producto.update({
+        where: {
+          id: producto.id,
+        },
+        data: {
+          cantidad: {
+            decrement: producto.cantidad,
+          },
+        },
+      });
+    });
     res.json({
+      message: "Vendido correctamente",
       data: venta,
-      message: "venta creada correctamente"
-    })
-  } catch (error) {
+    });
+  } catch (e) {
     res.status(500).json({
-      message: "Error al crear la venta",
-      error: error.message
-    })
+      message: "Error",
+      error: e.message,
+    });
   }
-})
+});
 export default app;
